b6d8cdde8c21b4f6461fa81b978ac61f2790c0be
pratik0502 <pratikjadhav0502@gmail.com>
2025-01-24 01:49:33 +0530

    add data_preprocesing.py

---
+++ machine_fault_prediction/src/data/data_preprocessing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import os
+from sklearn.preprocessing import StandardScaler
+
+# Load raw data
+train_data = pd.read_csv(r'D:\ML_ops\pratik\machine_fault\data\raw\train.csv')
+test_data = pd.read_csv(r'D:\ML_ops\pratik\machine_fault\data\raw\test.csv')
+
+# Standardize data
+scaler = StandardScaler()
+train_preprocess = scaler.fit_transform(train_data)
+test_preprocess = scaler.transform(test_data)
+
+# Convert back to DataFrame
+train_preprocess_df = pd.DataFrame(train_preprocess, columns=train_data.columns)
+test_preprocess_df = pd.DataFrame(test_preprocess, columns=test_data.columns)
+
+# Define the output directory
+data_path = os.path.join("data", "preprocessed_data")
+
+# Ensure the directory exists
+os.makedirs(data_path, exist_ok=True)
+
+# Save preprocessed data
+train_preprocess_df.to_csv(os.path.join(data_path, 'train_preprocess.csv'), index=False)
+test_preprocess_df.to_csv(os.path.join(data_path, 'test_preprocess.csv'), index=False)
+
+print(f"Preprocessed data saved to: {data_path}")
